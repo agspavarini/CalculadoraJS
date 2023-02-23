0c71c71d2a1cd96a7588994065ae151811ca51d7
Guilherme Moll <guilhermemoll.silva@philips.com>
2023-02-23 15:17:00 -0300

    atribuindo logica botoes e operacose basicas

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const hamburguerButton = document.getElementById("hamburguer-button");
 const calculatorModalElement = document.getElementById("calculator-modal");
 const closeModalButton = document.getElementById("close-modal");
-
+const keyboardContainer = document.getElementById("keyboard-container");
 const resourceDataStackElement = document.getElementById("middle");
-
+const equation = document.getElementById("equation")
+const equationResult = document.getElementById("equation-result")
 const historyTabElement = document.getElementById("history")
 const memoryTabElement = document.getElementById("memory")
 
@@ -101,3 +102,73 @@ memoryTabElement.addEventListener("click", (element) => {
     } 
 
 });
+
+function addBehaviour(){}
+
+let input = ['', '', '']
+let aux = 0
+const excludedButtons = ["C","CE","1/x","x²","√x","+/-",","]
+const operators = ['+', '-', 'x', '÷']
+
+for(let i = 1; i < keyboardContainer.childElementCount; i++){
+    let botao = keyboardContainer.children[i].children[0]
+
+    if(!excludedButtons.includes(botao.innerText)){
+        botao.addEventListener("click", () => {
+            let userInput = botao.innerText
+            
+            if (userInput == "=") {
+                runOperation()
+                // Pegar o resultado e joga na primeira posicao do array
+                // e espera receber outra operação
+                // aqui vai tambem a parte do historico dando push no array 
+            } else {
+                if (operators.includes(userInput)) {
+                    input[1] += userInput
+                    aux = 2
+                } else {
+                    input[aux] += userInput
+                }
+                displayValue()
+            }
+
+            console.log(input)
+
+
+        })
+    }
+
+}   
+
+function displayValue() {
+    equation.innerText = input.join(" ")
+}
+
+function clearDisplay(){
+    input = []
+    displayValue() 
+}
+
+
+function runOperation() {
+    let operando1 = Number(input[0])
+
+    let operador = input[1]
+
+    let operando2 = Number(input[2])
+
+    switch (operador) {
+        case "+":
+            equationResult.innerText = operando1 + operando2
+            break
+        case "-":
+            equationResult.innerText = operando1 - operando2
+            break
+        case "x":
+            equationResult.innerText = operando1 * operando2
+            break
+        case "÷":
+            equationResult.innerText = operando1 / operando2
+            break
+    }
+} 
